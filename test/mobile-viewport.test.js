c0cd282f6fff81941f26061e5a116dc7f4f8b4e9
djfjjd <djfjjd@djfjjdui-MacBookAir.local>
2026-09-16 15:56:52 +0900

    style: space other parking below tower

--- test/mobile-viewport.test.js
+++ test/mobile-viewport.test.js
@@ -92,6 +92,7 @@ test('전체 보기에서 6층 오른쪽에 13층·새싹·그외주차구역을
   assert.match(css,/data-map-zone="tower"\]\{[^}]*margin-top:-18px/);
   assert.match(css,/data-map-zone="auto13"\]\{grid-column:4\/span 3;grid-row:1\}/);
   assert.match(css,/data-map-zone="other-parking"\]\{grid-column:4\/span 3;grid-row:3;[^}]*margin-top:-18px\}/);
+  assert.match(css,/\.parking-map-stack>\.parking-map\[data-map-zone="other-parking"\]\{margin-top:126px\}/);
   assert.match(css,/data-map-zone="auto13"\] \.parking-map-scroll,.parking-map\[data-map-zone="other-parking"\] \.parking-map-scroll\{height:238px\}/);
   assert.match(css,/@media\(max-width:800px\)\{\.zones\.is-all\{position:relative;display:block\}/);
   assert.match(css,/\.zones\.is-all>\.parking-map-stack\{position:absolute;z-index:3;top:55px;right:0;width:50%/);
